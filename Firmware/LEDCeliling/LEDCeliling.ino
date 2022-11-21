126556789e81da72eacb53c43d965eebe7617f85
Cheng <swps9940902@gmail.com>
2022-11-22 03:03:09 +0800

    fork hub75

--- Firmware/LEDCeliling/LEDCeliling.ino
+++ Firmware/LEDCeliling/LEDCeliling.ino
@@ -1,6 +1,11 @@
-void setup(){
- pinMode(10,OUTPUT);
+#include "src/Adafruit_INA219/Adafruit_INA219.h"
+Adafruit_INA219 ina219;
+
+void setup()
+{
+    pinMode(10, OUTPUT);
 }
-void loop(){
-    digitalWrite(10,HIGH);
+void loop()
+{
+    digitalWrite(10, HIGH);
 }
